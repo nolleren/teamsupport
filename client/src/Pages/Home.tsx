16cb42018816316697a95d9a4c8f19a9881c052a
Stefan Østergaard <ste@relateit.dk>
2022-01-24 16:45:16 +0100

    added client again

---
+++ client/src/Pages/Home.tsx
@@ -0,0 +1,21 @@
+import { Stack } from '@fluentui/react'
+
+
+const Home = () => {
+
+	return (
+		<Stack horizontal >
+			<Stack.Item>
+
+			</Stack.Item>
+			<Stack.Item>
+
+			</Stack.Item>
+			<Stack.Item>
+
+			</Stack.Item>
+		</Stack>
+	)
+}
+
+export default Home;
