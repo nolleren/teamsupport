d5867dda2203d9efddfd279a6019f65083cae0d8
Stefan Østergaard <ste@relateit.dk>
2022-01-24 16:55:19 +0100

    refactored gitignore

--- API/BLL/Data/Answer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BLL.Data
-{
-    public class Answer : BaseEntity
-    {
-        public Guid TicketId { get; set; }
-        public Guid UserId { get; set; }
-        public string? Text { get; set; }
-    }
-}
